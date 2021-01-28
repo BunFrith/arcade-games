04fff4a751d4137541c7e56aa0117316b1de8a77
BunFrith <serj-solomko@mail.ru>
2021-01-28 15:02:49 +0200

    New variation of main display

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-jQuery(document).ready(function () {
-
+jQuery(document).ready(function ($) {
+  $(".main-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: ".main-slider__nav",
+  });
+  $(".main-slider__nav").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: ".main-slider",
+    centerMode: true,
+    focusOnSelect: true,
+  });
 });
