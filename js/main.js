cb2a5d620b96d8bf183b137f20dd238d470ea67f
BunFrith <serj-solomko@mail.ru>
2021-02-18 17:49:44 +0200

    Adding sliders to main page(stage 1)

--- js/main.js
+++ js/main.js
@@ -17,10 +17,26 @@ jQuery(document).ready(function ($) {
     focusOnSelect: true,
   });
 
-  $(".main-games").slick({
+  $(".main-top").slick({
+    infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
     centerMode: true,
-    arrows: false,
+    arrows: true,
+    variableWidth: true,
+    appendArrows: ".slick-arrows__top",
+    prevArrow: '<svg class="slick-arrows__top-prev" viewBox="0 0 9 14"><path class="svg-strelka" d="M6.660,8.922 L6.660,8.922 L2.350,13.408 L0.503,11.486 L4.813,7.000 L0.503,2.515 L2.350,0.592 L8.507,7.000 L6.660,8.922 Z" ></path></svg>',
+    nextArrow: '<svg class="slick-arrows__top-next" viewBox="0 0 9 14"><path class="svg-strelka" d="M6.660,8.922 L6.660,8.922 L2.350,13.408 L0.503,11.486 L4.813,7.000 L0.503,2.515 L2.350,0.592 L8.507,7.000 L6.660,8.922 Z" ></path></svg>',
+  });
+  $(".main-new").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    centerMode: true,
+    arrows: true,
+    variableWidth: true,
+    appendArrows: ".slick-arrows__new",
+    prevArrow: '<button type="button" class="main-games__item-prev"><</button>',
+    nextArrow: '<button type="button" class="main-games__item-next">></button>',
   });
 });
